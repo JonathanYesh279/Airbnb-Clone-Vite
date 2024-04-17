2852ac934d86ad813ec368fac5c936eceedd830a
Jonathan_Yeshayahu <73723174+JonathanYesh279@users.noreply.github.com>
2024-04-17 16:52:54 +0300

    src commit

---
+++ src/Perks.jsx
@@ -0,0 +1,124 @@
+
+function Perks({selected, onChange}) {
+  return (
+    <>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z'
+          />
+        </svg>
+        <span>Wifi</span>
+      </label>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z'
+          />
+        </svg>
+        <span>Phone</span>
+      </label>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z'
+          />
+        </svg>
+        <span>TV</span>
+      </label>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z'
+          />
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z'
+          />
+        </svg>
+
+        <span>Fire alarm</span>
+      </label>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25'
+          />
+        </svg>
+        <span>Computer</span>
+      </label>
+      <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
+        <input type='checkbox' />
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='m3.75 7.5 16.5-4.125M12 6.75c-2.708 0-5.363.224-7.948.655C2.999 7.58 2.25 8.507 2.25 9.574v9.176A2.25 2.25 0 0 0 4.5 21h15a2.25 2.25 0 0 0 2.25-2.25V9.574c0-1.067-.75-1.994-1.802-2.169A48.329 48.329 0 0 0 12 6.75Zm-1.683 6.443-.005.005-.006-.005.006-.005.005.005Zm-.005 2.127-.005-.006.005-.005.005.005-.005.005Zm-2.116-.006-.005.006-.006-.006.005-.005.006.005Zm-.005-2.116-.006-.005.006-.005.005.005-.005.005ZM9.255 10.5v.008h-.008V10.5h.008Zm3.249 1.88-.007.004-.003-.007.006-.003.004.006Zm-1.38 5.126-.003-.006.006-.004.004.007-.006.003Zm.007-6.501-.003.006-.007-.003.004-.007.006.004Zm1.37 5.129-.007-.004.004-.006.006.003-.004.007Zm.504-1.877h-.008v-.007h.008v.007ZM9.255 18v.008h-.008V18h.008Zm-3.246-1.87-.007.004L6 16.127l.006-.003.004.006Zm1.366-5.119-.004-.006.006-.004.004.007-.006.003ZM7.38 17.5l-.003.006-.007-.003.004-.007.006.004Zm-1.376-5.116L6 12.38l.003-.007.007.004-.004.007Zm-.5 1.873h-.008v-.007h.008v.007ZM17.25 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Zm0 4.5a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z'
+          />
+        </svg>
+
+        <span>Radio</span>
+      </label>
+    </>
+  );
+}
+
+export default Perks
--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import Perks from '../Perks';
 
 function PlacesPage() {
   const { action } = useParams();
+  const [title, setTitle] = useState('');
+  const [address, setAddress] = useState('');
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState('');
+  const [description, setDescription] = useState('');
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState('');
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut, setCheckOut] = useState('');
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  function inputHeader(text) {
+    return <h2 className='text-2xl mt-4'>{text}</h2>
+  }
+
+  function inputDescription(text) {
+    return <p className='text-gray-500 text-sm'>{text}</p>
+  }
+  
+  function preInput(header, description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+
+      </>
+    )
+  }
+
+  async function addPhotoByLink(ev) {
+    ev.preventDefault();
+    const { data: filename } = await axios.post('/upload-by-link', { link: photoLink });
+    setAddedPhotos(prev => {
+      return [...prev, filename];
+    });
+    setPhotoLink('');
+  }
+
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append('photos', files[i]);
+    } 
+    axios.post('/upload', data, {
+      headers: {'Content-Type':'multipart/from-data'}
+    }).then(response => {
+      const { data:filenames } = response;
+      setAddedPhotos((prev) => {
+        return [...prev, ...filenames];
+      });
+    })
+  }
+
   return (
     <div>
       {action !== 'new' && (
@@ -32,25 +88,50 @@ function PlacesPage() {
       {action === 'new' && (
         <div>
           <form>
-            <h2 className='text-2xl mt-4'>Title</h2>
-            <p className='text-gray-500 text-sm'>title for your place.</p>
+            {preInput('Title', 'title for your place.')}
             <input
               type='text'
               placeholder='title, for example: My lovely apt'
+              value={title}
+              onChange={(ev) => setTitle(ev.target.value)}
             />
-            <h2 className='text-2xl mt-4'>Address</h2>
-            <p className='text-gray-500 text-sm'>Address to this place.</p>
-            <input type='text' placeholder='address' />
-            <h2 className='text-2xl mt-4'>Photos</h2>
-            <p className='text-gray-500 text-sm'>Add some photos.</p>
+            {preInput('Address', 'Address to this place.')}
+            <input
+              type='text'
+              placeholder='address'
+              value={address}
+              onChange={(ev) => setAddress(ev.target.value)}
+            />
+            {preInput('Photos', 'Add some photos.')}
             <div className='flex gap-2'>
-              <input type='text' placeholder={'Add using a link ....jpg'} />
-              <button className='bg-gray-200 px-4 rounded-2xl'>
+              <input
+                type='text'
+                placeholder={'Add using a link ....jpg'}
+                value={photoLink}
+                onChange={(ev) => setPhotoLink(ev.target.value)}
+              />
+              <button
+                onClick={addPhotoByLink}
+                className='bg-gray-200 px-4 rounded-2xl'
+              >
                 Add&nbsp;photo
               </button>
             </div>
-            <div className='mt-2 grid grid-cols-3 lg:grid-cols-6 md:grid-cols-4'>
-              <button className='flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600'>
+            <div className='mt-2 grid gap-2 grid-cols-3 lg:grid-cols-6 md:grid-cols-4'>
+              {addedPhotos.length > 0 &&
+                addedPhotos.map((link, index) => (
+                  <div
+                    className='h-32 flex '
+                    key={index}>
+                    <img
+                      className='rounded-2xl w-full object-cover'
+                      src={'http://localhost:5000/uploads/' + link}
+                      alt='picture'
+                    />
+                  </div>
+                ))}
+              <label className='flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 cursor-pointer h-32'>
+                <input type='file' multiple className='hidden' onChange={uploadPhoto} />
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
@@ -66,149 +147,49 @@ function PlacesPage() {
                   />
                 </svg>
                 Upload
-              </button>
+              </label>
             </div>
-            <h2 className='text-2xl mt-4'>Description</h2>
-            <p className='text-gray-500 text-sm'>description of the place</p>
-            <textarea />
-            <h2 className='text-2xl mt-4'>Perks</h2>
-            <p className='text-gray-500 text-sm'>
-              select all the perks of your place
-            </p>
+            {preInput('Description', 'description of the place')}
+            <textarea
+              value={description}
+              onChange={(ev) => setDescription(ev.target.value)}
+            />
+            {preInput('Perks', 'select all the perks of your place')}
             <div className='grid mt-2 gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z'
-                  />
-                </svg>
-                <span>Wifi</span>
-              </label>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z'
-                  />
-                </svg>
-                <span>Phone</span>
-              </label>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z'
-                  />
-                </svg>
-                <span>TV</span>
-              </label>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z'
-                  />
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z'
-                  />
-                </svg>
-
-                <span>Fire alarm</span>
-              </label>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25'
-                  />
-                </svg>
-                <span>Computer</span>
-              </label>
-              <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type='checkbox' />
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='w-6 h-6'
-                >
-                  <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='m3.75 7.5 16.5-4.125M12 6.75c-2.708 0-5.363.224-7.948.655C2.999 7.58 2.25 8.507 2.25 9.574v9.176A2.25 2.25 0 0 0 4.5 21h15a2.25 2.25 0 0 0 2.25-2.25V9.574c0-1.067-.75-1.994-1.802-2.169A48.329 48.329 0 0 0 12 6.75Zm-1.683 6.443-.005.005-.006-.005.006-.005.005.005Zm-.005 2.127-.005-.006.005-.005.005.005-.005.005Zm-2.116-.006-.005.006-.006-.006.005-.005.006.005Zm-.005-2.116-.006-.005.006-.005.005.005-.005.005ZM9.255 10.5v.008h-.008V10.5h.008Zm3.249 1.88-.007.004-.003-.007.006-.003.004.006Zm-1.38 5.126-.003-.006.006-.004.004.007-.006.003Zm.007-6.501-.003.006-.007-.003.004-.007.006.004Zm1.37 5.129-.007-.004.004-.006.006.003-.004.007Zm.504-1.877h-.008v-.007h.008v.007ZM9.255 18v.008h-.008V18h.008Zm-3.246-1.87-.007.004L6 16.127l.006-.003.004.006Zm1.366-5.119-.004-.006.006-.004.004.007-.006.003ZM7.38 17.5l-.003.006-.007-.003.004-.007.006.004Zm-1.376-5.116L6 12.38l.003-.007.007.004-.004.007Zm-.5 1.873h-.008v-.007h.008v.007ZM17.25 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Zm0 4.5a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z'
-                  />
-                </svg>
-
-                <span>Radio</span>
-              </label>
+              <Perks selected={perks} onChange={setPerks} />
             </div>
-            <h2 className='text-2xl mt-4'>Extra info</h2>
-            <p className='text-gray-500 text-sm'>house rules, etc</p>
-            <textarea />
-            <h2 className='text-2xl mt-4'>Check in&out times</h2>
-            <p className='text-gray-500 text-sm'>add check in and out</p>
+            {preInput('Extra info', 'house rules, etc')}
+            <textarea
+              value={extraInfo}
+              onChange={(ev) => setExtraInfo(ev.target.value)}
+            />
+            {preInput('Check in&out times', 'add check in and out')}
             <div className='grid gap-2 sm:grid-cols-3'>
               <div>
                 <h3 className='mt-2 -mb-1'>Check in time</h3>
-                <input type='text' placeholder='14:00PM' />
+                <input
+                  type='text'
+                  placeholder='14:00PM'
+                  value={checkIn}
+                  onChange={(ev) => setCheckIn(ev.target.value)}
+                />
               </div>
               <div>
                 <h3 className='mt-2 -mb-1'>Check out time</h3>
-                <input type='text' placeholder='11:00AM' />
+                <input
+                  type='text'
+                  placeholder='11:00AM'
+                  value={checkOut}
+                  onChange={(ev) => setCheckOut(ev.target.value)}
+                />
               </div>
               <div>
                 <h3 className='mt-2 -mb-1'>Max number of guests</h3>
-                <input type='text' />
+                <input
+                  type='number'
+                  value={maxGuests}
+                  onChange={(ev) => setMaxGuests(ev.target.value)}
+                />
               </div>
             </div>
             <button className='primary my-4'>Save</button>
